8356bcd5d17d19bfb27efb2565ebe421dd8c54d9
nandita145 <92240425+nandita145@users.noreply.github.com>
2022-09-20 19:47:17 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb://localhost:27017/nodemongo2", { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect("mongodb://0.0.0.0:27017/nodemongo2", { useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> console.log('connection successful'))
     .catch((err)=>console.log(err));
